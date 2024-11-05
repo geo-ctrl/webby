49435e13937d014fa8fe719e1ad9bf6db18e4b37
geo-ctrl <gkanyi56@gmail.com>
2024-11-05 14:23:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('change-color-btn').addEventListener('click', function() {
+    // Change the background color of the body element
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? '#f0f0f0' : 'lightblue';
+});
